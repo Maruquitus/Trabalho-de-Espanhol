92afca0e8f231e506ed759b4bab21c319bfcd131
Maruquitus <killerredstone1@gmail.com>
2022-11-09 21:55:47 -0300

    Atualização de telas

---
+++ static/script.js
@@ -0,0 +1,18 @@
+/*redirecionamento de telas*/
+
+function proximo() {
+    window.location.href = "explicacao"
+}
+
+function voltar() {
+    window.location.href = "inicio"
+}
+
+function voltarBtn() {
+    window.location.href = "explicacao"
+}
+
+function proximoDois() {
+    window.location.href = "questao"
+}
+
