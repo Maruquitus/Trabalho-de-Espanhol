8976d644aca8eab324979eba6dd901fd97c3fcbb
Maruquitus <killerredstone1@gmail.com>
2022-11-09 21:51:11 -0300

    Adicionadas telas de explicação e variações novas de questão +bugfixes

--- app.py
+++ app.py
@@ -1,119 +1,205 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import random
 import conjugador
-import time
 
 app = Flask(__name__)
-DADOS_QUESTÕES = {
+DADOS_QUESTÕES = { #Definição das perguntas para serem escolhidas
     1:{
-        "pergunta":0,
-        "frase":"Yo ______ mucho",
+        "pergunta":0, #Tipo do enunciado
+        "frase":"Yo ______ mucho.",
         "verbo":"Hablar",
         "resposta certa":"Hablo"
     },
 
     2:{
         "pergunta":0,
-        "frase":"Tú _______ bién",
+        "frase":"Tú _______ bién.",
         "verbo":"Cantar",
         "resposta certa":"Cantas"
     },
 
     3:{
         "pergunta":0,
-        "frase":"El maestro _______ a los estudiantes",
+        "frase":"El maestro _______ a los estudiantes.",
         "verbo":"Educar",
         "resposta certa":"Educa"
     },
 
     4:{
         "pergunta":0,
-        "frase":"Mi amigo siempre _____ en los juegos",
+        "frase":"Mi amigo siempre _____ en los juegos.",
         "verbo":"Ganar",
         "resposta certa":"Gana"
     },
     5:{
         "pergunta":0,
-        "frase":"La activista ______ la causa",
+        "frase":"La activista ______ la causa.",
         "verbo":"Apoyar",
         "resposta certa":"Apoya"
+    },
+    6:{
+        "pergunta":0,
+        "frase":"Los alumnos __________ español.",
+        "verbo":"Estudiar",
+        "resposta certa": "Estudian"
+    },
+    7:{
+        "pergunta":0,
+        "frase":"María _________ bien el contenido.",
+        "verbo":"Presentar",
+        "resposta certa": "Presenta"
+    },
+    8:{
+        "pergunta":0,
+        "frase":"Me ______ el jugo de piña.",
+        "verbo":"Gustar",
+        "resposta certa": "Gusta"
+    },
+    9:{
+        "pergunta":0,
+        "frase":"¿Tu _________ con su madre todos los días?",
+        "verbo":"Caminar",
+        "resposta certa": "Caminas"
+    },
+    10:{
+        "pergunta":0,   
+        "frase":"Mis amigos _________ musica.",
+        "verbo":"Escuchar",
+        "resposta certa": "Escuchan"
+    },
+    11:{
+        "pergunta":0,   
+        "frase":"Siempre _____ al cielo por la noche porque la luna es hermosa.",
+        "verbo":"Mirar",
+        "resposta certa": "Miro"
+    },
+    12:{
+        "pergunta":0,   
+        "frase":"Ellos ______ a sus amigos cada vez que tienen una fiesta.",
+        "verbo":"Invitar",
+        "resposta certa": "Invitan"
+    },
+    13:{
+        "pergunta":0,   
+        "frase":"Yo ________ de ayuda para terminar la actividad.",
+        "verbo":"Necesitar",
+        "resposta certa": "Necesito"
+    },
+    14:{
+        "pergunta":0,   
+        "frase":"Miguel ______ paisajes muy bonitos.",
+        "verbo":"Dibujar",
+        "resposta certa": "Dibuja"
+    },
+    15:{
+        "pergunta":0,   
+        "frase":"Cuando ________ el año nuevo, llamo a todos los miembros de mi familia.",
+        "verbo":"Celebrar",
+        "resposta certa": "Celebro"
     }
-
 }
-TIPOS_PERGUNTAS = ["Completa la oración con la conjugación correcta del verbo"]
+""",
+    8:{ #Tipo de questão para ser implementado futuramente
+        "pergunta":1,
+        "frases": ["Los hermanos se aman.",
+        "Todas las vidas importas.",
+        "Ellos tomaron una   decisión.",
+        "Mi abuela cocino bién."
+        ],
+        "frase certa": "Los hermanos se aman."
+    }"""
+TIPOS_PERGUNTAS = ["Completa la oración con la conjugación correcta del verbo", 
+"Marca la oración en la que la conjugación del verbo en el presente del indicativo es correcta"]
 
 acertos = 0
 q = 1
-
-@app.route('/')
-def home():
-    return render_template("telaquestao.html", enunciado="Padrão", itens=["Letra A", "Letra B", "Letra C", "Letra D"], resultado = "Nada", estado = ["", "", "", ""])
-
 questoesVistas = []
 respCerta = ""
 estado = "    "
+qatual = 0
 
 def novaQuestao():
-    global q
-    global opcoes
-    global escolhidas
-    global estado
+    global q, qatual, opcoes, escolhidas, estado
     global questoesVistas
 
-    while q in questoesVistas:
+    qatual += 1
+
+    while q in questoesVistas: #Criar uma nova questão ainda não vista
         q = random.randint(1, len(DADOS_QUESTÕES.keys()))
         if len(questoesVistas) == len(DADOS_QUESTÕES.keys()):
-            questoesVistas = []
+            questoesVistas = [] #Resetar para começar a repetir questões
+            ###Adicionar tela final
     questoesVistas.append(q)
 
     pergunta, frase, verbo, respCerta = DADOS_QUESTÕES[q].values()
 
-    opcoes = conjugador.conjugacoes(DADOS_QUESTÕES[q]['verbo'])
+    opcoes = conjugador.conjugacoes(DADOS_QUESTÕES[q]['verbo']) #Gerar as opções de conjugação
     escolhidas = []
     ic = opcoes.index(respCerta)
-    while len(escolhidas) < 4:
+    while len(escolhidas) < 4: #Gerar itens aleatórios de acordo com as conjugações do verbo
         r = random.randint(0, len(opcoes)-1)
         if r not in escolhidas:
             escolhidas.append(r)
-        if len(escolhidas) == 4 and ic not in escolhidas:
+        if len(escolhidas) == 4 and ic not in escolhidas: #Caso a opção correta não tenha sido gerada, substituir um dos itens por ela
             escolhidas[random.randint(0, 3)] = ic
     
     estado = []
     altCorreta = "ABCD"[escolhidas.index(ic)]
     for i in "ABCD":
-        if i == altCorreta:
+        if i == altCorreta: #Atualizar estado para fins do css
             estado.append("certa")
         else:
             estado.append("")
 
-    print(questoesVistas)
-    print(respCerta)
-
     return pergunta, frase, verbo, altCorreta
+
+#Roteamento
+@app.route('/')
+def home():
+    return redirect('/inicio') #Redirecionar a página vazia para o início
+
+@app.route('/inicio', methods =["GET", "POST"])
+def inicio():
+    return render_template("inicio.html")
+
+@app.route('/explicacao', methods =["GET", "POST"])
+def explicacao():
+    global qatual, acertos, questoesVistas
+    #Resetar valores na página de início
+    acertos = 0
+    qatual = 0
+    questoesVistas = []
+    return render_template("explicacao.html")
 
 @app.route('/questao', methods =["GET", "POST"])
 def loop():
-    global q
-    global opcoes
-    global escolhidas
-    global estado
-    global acertos
-    global respCerta
+    global q, qatual, opcoes, escolhidas
+    global estado, acertos, respCerta
+    global pergunta, frase, verbo, respCerta
 
+    passar = False #Variável para impedir a geração de novas questões com o refresh
     if request.method == "GET":
-        pergunta, frase, verbo, respCerta = novaQuestao()
-        return render_template("telaquestao.html", enunciado=f"{TIPOS_PERGUNTAS[pergunta]} {verbo}: {frase}", itens=[opcoes[escolhidas[0]], opcoes[escolhidas[1]], opcoes[escolhidas[2]], opcoes[escolhidas[3]]], estado=estado, acertos=acertos)
+        passar = True
+        if qatual == 0:
+            pergunta, frase, verbo, respCerta = novaQuestao()
 
-    if request.method == "POST":
-        alts = "ABCD"
-        alternativa = list(request.form.keys())[0]
+    if request.method == "POST" or passar: #Quando uma alternativa é escolhida
+        if passar:
+            alternativa = ""
+        else:
+            alternativa = list(request.form.keys())[0] #Identificar a alternativa marcada
+            if alternativa == respCerta: #Verificar se está correta
+                acertos += 1
 
-        if alternativa == respCerta:
-            acertos += 1
+            pergunta, frase, verbo, respCerta = novaQuestao() #Gerar nova questão
 
-        pergunta, frase, verbo, respCerta = novaQuestao()
-
-        return render_template("telaquestao.html", enunciado=f"{TIPOS_PERGUNTAS[pergunta]} {verbo}: {frase}", itens=[opcoes[escolhidas[0]], opcoes[escolhidas[1]], opcoes[escolhidas[2]], opcoes[escolhidas[3]]], estado=estado, acertos=acertos)
+        return render_template("telaquestao.html", 
+        qatual=qatual, 
+        enunciado=f"{TIPOS_PERGUNTAS[pergunta]} {verbo}: {frase}", 
+        itens=[opcoes[escolhidas[0]], opcoes[escolhidas[1]], 
+        opcoes[escolhidas[2]], opcoes[escolhidas[3]]], 
+        estado=estado, 
+        acertos=acertos)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- conjugador.py
+++ conjugador.py
@@ -5,8 +5,7 @@ def conjugar(palavra, pessoa, numero):
     resultado = "NÃO ENCONTRADO"
     for t in TERMINAÇÕES:
         if id == ind:
-            resultado = palavra.replace("har", t)
-            resultado = resultado.replace("ar", t)
+            resultado = palavra.replace("ar", t)
             break
         ind += 1
     return resultado
@@ -22,27 +21,3 @@ def conjugacoes(palavra):
         resultado.append((conjugar(palavra, ind, 'plural')))
         ind += 1
     return resultado
-
-"""
-while True:
-    i = input("Conjugar (-ar) >>> ")
-
-    if i == "stop":
-        break
-    else:
-        s = i.split("/")
-        r = conjugador(s[0], int(s[1]), s[2])
-
-        print(f"Resultado -> {r}")
-        print(f"* {s[1]}ª pessoa do {s[2]}")
-        print("- Singular")
-        ind = 0
-        for p in ["Yo", "Tu", "Él / Ella / Usted"]:
-            print(f"{p} {conjugador(i, ind, 'singular')}")
-            ind += 1
-        ind = 0
-        print("- Plural")
-        for p in ["Nosotros/as", "Vosostros/as", "Ellos / Ellas/ Ustedes"]:
-            print(f"{p} {conjugador(i, ind, 'plural')}")
-            ind += 1
-"""
